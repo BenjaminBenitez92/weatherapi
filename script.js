c7aec5d2b2e7b6cc01ee0bc509cdd56fad25630f
BenjaminBenitez92 <81438491+BenjaminBenitez92@users.noreply.github.com>
2021-07-12 21:18:32 -0400

    that is all youre getting

---
+++ script.js
@@ -0,0 +1,22 @@
+const key = "2RIlFOUyyad42MbblKaq71EJARHMxgCN";
+// get weather info
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+// get city info
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
